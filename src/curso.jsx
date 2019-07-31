fb3f5c942b10fd064ba96b17d5c48c2e247ffe48
Daniel Fragoso <ing.fragoso94@gmail.com>
2019-07-30 23:14:32 -0500

    Se agrego propiedades y expresiones al componente curso

--- src/curso.jsx
+++ src/curso.jsx
@@ -1,15 +1,31 @@
 import React from 'react';
 import "./styles/style.scss";
 
+
+//función JS ES6
+//const mayorDeEdad = edad => edad >18;
+//const persona = {"nombre": "daniel fragoso", "edad": 18};
+const curso =
+    {
+        "titulo": "Curso de react",
+        "img": "https://drupal.ed.team/sites/default/files/imagenes-cdn-edteam/2019-04/React%20desde%20cero%20%281%29.png",
+        "precio": "250 MXN"
+    }
 const Curso = () => (
     <article classNameName="card">
         <div classNameName="img-container s-ratio-16-9 s-radius-tr s-radius-tl">
-          <img src="path/img" />
+          <img src={curso.img} alt={curso.titulo} />
         </div>
         <div className="card__data s-border s-radius-br s-radius-bl s-pxy-2">
-          <h3 className="t5 s-mb-2 s-center">
-            Programación orientada a objetos con Go
-          </h3>
+            <h3 className="t5 s-mb-2 s-center">
+                {curso.titulo}
+            </h3>
+            {/*<h3 className="t5 s-mb-2 s-center">
+              {`Autor ${persona.nombre}`}
+            </h3>
+            <h4>
+                { mayorDeEdad(persona.edad) ? "Soy mayor de edad!" : "No soy mayor de edad."}
+            </h4>*/}
         <div className="s-mb-2 s-main-center">
           <div className="card__teacher s-cross-center">
             <div className="card__avatar s-mr-1">
@@ -17,11 +33,11 @@ const Curso = () => (
                 <img src="path/img" />
               </div>
             </div>
-            <span className="small">Alexys Lozada</span>
+            <span className="small">{}</span>
           </div>
         </div>
         <div className="s-main-center">
-          <a className="button--ghost-alert button--tiny">$ 20USD</a>
+          <a className="button--ghost-alert button--tiny">{`$ ${curso.precio}`}</a>
         </div>
       </div>
     </article>
